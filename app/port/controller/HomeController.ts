d6801b7e4ae1cba166e5b8adb989a63612afd6cc
fengmk2 <fengmk2@gmail.com>
2021-11-21 01:54:16 +0800

    feat: impl GET /

---
+++ app/port/controller/HomeController.ts
@@ -0,0 +1,65 @@
+import { performance } from 'perf_hooks';
+import {
+  HTTPController,
+  HTTPMethod,
+  HTTPMethodEnum,
+  Context,
+  EggContext,
+} from '@eggjs/tegg';
+import { BaseController } from '../type/BaseController';
+
+@HTTPController()
+export class HomeController extends BaseController {
+  @HTTPMethod({
+    // GET /
+    // https://github.com/cnpm/cnpmjs.org/blob/master/docs/registry-api.md#schema
+    path: '/',
+    method: HTTPMethodEnum.GET,
+  })
+  async showTotal(@Context() ctx: EggContext) {
+    const data = {
+      db_name: 'registry',
+      engine: ctx.app.config.orm.client,
+      doc_count: 0,
+      doc_del_count: 0,
+      update_seq: 0,
+      purge_seq: 0,
+      compact_running: false,
+      sizes: {
+        active: 0,
+        external: 0,
+        file: 0,
+      },
+      disk_size: 0,
+      data_size: 0,
+      other: {
+        data_size: 0,
+      },
+      instance_start_time: Math.floor((Date.now() - performance.now()) * 1000),
+      disk_format_version: 0,
+      committed_update_seq: 0,
+      compacted_seq: 0,
+      uuid: '',
+      // only for cnpmcore
+      doc_version_count: 0,
+      sync_model: '',
+      last_sync_time: 0,
+      last_exist_sync_time: 0,
+      last_sync_module: 0,
+      download: {
+        today: 0,
+        thisweek: 0,
+        thismonth: 0,
+        lastday: 0,
+        lastweek: 0,
+        lastmonth: 0,
+        total: 0,
+      },
+      node_version: process.version,
+      app_version: ctx.app.config.pkg.version,
+      // donate: 'https://github.com/cnpm/cnpmcore',
+      cache_time: 0,
+    };
+    return data;
+  }
+}
---
+++ test/port/controller/HomeController.test.ts
@@ -0,0 +1,31 @@
+import { strict as assert } from 'assert';
+import { Context } from 'egg';
+import { app, mock } from 'egg-mock/bootstrap';
+
+describe('test/controller/PackageController.test.ts', () => {
+  let ctx: Context;
+  beforeEach(async () => {
+    ctx = await app.mockModuleContext();
+  });
+
+  afterEach(async () => {
+    app.destroyModuleContext(ctx);
+    mock.restore();
+  });
+
+  describe('[GET /] showTotal()', () => {
+    it('should total information', async () => {
+      const res = await app.httpRequest()
+        .get('/')
+        .expect(200)
+        .expect('content-type', 'application/json; charset=utf-8');
+      const data = res.body;
+      // console.log(data);
+      assert.equal(data.engine, app.config.orm.client);
+      assert.equal(data.node_version, process.version);
+      assert(data.instance_start_time > 0);
+      // microseconds
+      assert.equal(String(data.instance_start_time).length, 16);
+    });
+  });
+});
