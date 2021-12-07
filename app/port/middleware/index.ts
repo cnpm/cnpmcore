07e7e35319734aced4b3d533201c4eb1dc3aa5d4
fengmk2 <fengmk2@gmail.com>
2021-12-07 16:22:51 +0800

    f

--- app/port/middleware/index.ts
+++ app/port/middleware/index.ts
@@ -1,9 +1,9 @@
 import { Middleware } from '@eggjs/tegg';
 import { AlwaysAuth } from './AlwaysAuth';
-import { errorHandler } from './ErrorHandler';
+import { ErrorHandler } from './ErrorHandler';
 import { Tracing } from './Tracing';
 
 @Middleware(AlwaysAuth)
 @Middleware(Tracing)
-@Middleware(errorHandler)
+@Middleware(ErrorHandler)
 export abstract class MiddlewareController {}
