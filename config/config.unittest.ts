92a208111ba3610b1b2d42e2f278cc020f1e88f2
fengmk2 <fengmk2@gmail.com>
2021-12-09 22:13:18 +0800

    fix: use nfs readBytes (#20)

--- config/config.unittest.ts
+++ config/config.unittest.ts
@@ -1,7 +1,10 @@
+import { join } from 'path';
+import { tmpdir } from 'os';
 import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config = {} as PowerPartial<EggAppConfig>;
+  config.dataDir = join(process.env.HOME || tmpdir(), '.cnpmcore_unittest');
 
   config.orm = {
     database: process.env.MYSQL_DATABASE || 'cnpmcore_unittest',
