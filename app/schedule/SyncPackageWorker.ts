c42f38ab5cbf38288945cd5fb8eeaf231a6b4f81
fengmk2 <fengmk2@gmail.com>
2021-12-17 22:22:41 +0800

    🐛 FIX: Should start span before execute task

--- app/schedule/SyncPackageWorker.ts
+++ app/schedule/SyncPackageWorker.ts
@@ -21,23 +21,29 @@ export default class SyncPackageWorker extends Subscription {
       const packageSyncerService: PackageSyncerService = ctx.module[cnpmcoreCore].packageSyncerService;
       executingCount++;
       let task: Task | null = null;
+      const span = ctx.tracer.startSpan('execute_task');
+      const startTime = Date.now();
       try {
         task = await packageSyncerService.findExecuteTask();
         if (!task) {
           return;
         }
-        const startTime = Date.now();
         ctx.logger.info('[SyncPackageWorker:subscribe:executeTask:start][%s] taskId: %s, targetName: %s, attempts: %s, params: %j, updatedAt: %s, delay %sms',
           executingCount, task.taskId, task.targetName, task.attempts, task.data, task.updatedAt,
           startTime - task.updatedAt.getTime());
         await packageSyncerService.executeTask(task);
+        const use = Date.now() - startTime;
         ctx.logger.info('[SyncPackageWorker:subscribe:executeTask:success][%s] taskId: %s, targetName: %s, use %sms',
-          executingCount, task.taskId, task.targetName, Date.now() - startTime);
+          executingCount, task.taskId, task.targetName, use);
+        span.log({ event: 'task_success', task_id: task.taskId, target_name: task.targetName, use });
       } catch (err) {
-        ctx.logger.info('[SyncPackageWorker:subscribe:executeTask:error][%s] taskId: %s, targetName: %s, %s',
+        ctx.logger.error('[SyncPackageWorker:subscribe:executeTask:error][%s] taskId: %s, targetName: %s, %s',
           executingCount, task && task.taskId, task && task.targetName, err);
+        const use = Date.now() - startTime;
+        span.log({ event: 'task_error', task_id: task && task.taskId, target_name: task && task.targetName, use });
       } finally {
         executingCount--;
+        span.finish();
       }
     });
   }
