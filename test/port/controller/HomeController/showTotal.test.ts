7ade15bf3e1815a666373d10a4d96bbdbb5ad3c6
fengmk2 <fengmk2@gmail.com>
2022-01-09 19:21:18 +0800

    f

--- test/port/controller/HomeController/showTotal.test.ts
+++ test/port/controller/HomeController/showTotal.test.ts
@@ -9,6 +9,9 @@ describe('test/port/controller/HomeController/showTotal.test.ts', () => {
   let ctx: Context;
   beforeEach(async () => {
     ctx = await app.mockModuleContext();
+    // make sure cache counter data cleanup
+    await app.runSchedule('SavePackageVersionDownloadCounter');
+    await TestUtil.truncateDatabase();
   });
 
   afterEach(() => {
@@ -17,15 +20,10 @@ describe('test/port/controller/HomeController/showTotal.test.ts', () => {
 
   describe('[GET /] showTotal()', () => {
     it('should total information', async () => {
-      await TestUtil.truncateDatabase();
       let res = await app.httpRequest()
         .get('/');
       assert(res.status === 200);
       assert(res.headers['content-type'] === 'application/json; charset=utf-8');
-      // ignore on oss, it was unstable
-      // https://github.com/cnpm/cnpmcore/runs/4748567783?check_suite_focus=true#step:6:748
-      if (process.env.CNPMCORE_NFS_TYPE === 'oss') return;
-
       let data = res.body;
       assert(data.doc_count === 0);
       assert(data.doc_version_count === 0);
