f89877fde2fa003b5b82f41012f34e73be6c5ac5
fengmk2 <fengmk2@gmail.com>
2022-02-19 12:10:56 +0800

    👌 IMPROVE: Compatible electron binary sync (#176)
    support both `/vX.Y.Z/` and `/X.Y.Z/` dir prefix
    
    - closes https://github.com/cnpm/cnpmjs.org/issues/1712
    - closes https://github.com/cnpm/cnpmjs.org/issues/1588

--- app/common/adapter/binary/GithubBinary.ts
+++ app/common/adapter/binary/GithubBinary.ts
@@ -3,7 +3,7 @@ import { AbstractBinary, FetchResult, BinaryItem } from './AbstractBinary';
 export class GithubBinary extends AbstractBinary {
   private releases?: any[];
 
-  async fetch(dir: string): Promise<FetchResult | undefined> {
+  protected async initReleases() {
     if (!this.releases) {
       // https://docs.github.com/en/rest/reference/releases get three pages
       // https://api.github.com/repos/electron/electron/releases
@@ -21,10 +21,54 @@ export class GithubBinary extends AbstractBinary {
       }
       this.releases = releases;
     }
+    return this.releases;
+  }
 
+  protected formatItems(releaseItem: any) {
     const items: BinaryItem[] = [];
+    // 200MB
+    const maxFileSize = 1024 * 1024 * 200;
+    for (const asset of releaseItem.assets) {
+      if (asset.size > maxFileSize) continue;
+      items.push({
+        name: asset.name,
+        isDir: false,
+        url: asset.browser_download_url,
+        size: asset.size,
+        date: asset.updated_at,
+      });
+    }
+    // tarball_url and zipball_url
+    // https://github.com/electron/electron/archive/v16.0.5.zip
+    // https://github.com/electron/electron/archive/v16.0.5.tar.gz
+    if (releaseItem.tarball_url) {
+      items.push({
+        name: `${releaseItem.tag_name}.tar.gz`,
+        isDir: false,
+        url: `https://github.com/${this.binaryConfig.repo}/archive/${releaseItem.tag_name}.tar.gz`,
+        size: '-',
+        date: releaseItem.published_at,
+      });
+    }
+    if (releaseItem.zipball_url) {
+      items.push({
+        name: `${releaseItem.tag_name}.zip`,
+        isDir: false,
+        url: `https://github.com/${this.binaryConfig.repo}/archive/${releaseItem.tag_name}.zip`,
+        size: '-',
+        date: releaseItem.published_at,
+      });
+    }
+    return items;
+  }
+
+  async fetch(dir: string): Promise<FetchResult | undefined> {
+    const releases = await this.initReleases();
+    if (!releases) return;
+
+    let items: BinaryItem[] = [];
     if (dir === '/') {
-      for (const item of this.releases) {
+      for (const item of releases) {
         items.push({
           name: `${item.tag_name}/`,
           isDir: true,
@@ -34,41 +78,9 @@ export class GithubBinary extends AbstractBinary {
         });
       }
     } else {
-      for (const item of this.releases) {
+      for (const item of releases) {
         if (dir === `/${item.tag_name}/`) {
-          // 200MB
-          const maxFileSize = 1024 * 1024 * 200;
-          for (const asset of item.assets) {
-            if (asset.size > maxFileSize) continue;
-            items.push({
-              name: asset.name,
-              isDir: false,
-              url: asset.browser_download_url,
-              size: asset.size,
-              date: asset.updated_at,
-            });
-          }
-          // tarball_url and zipball_url
-          // https://github.com/electron/electron/archive/v16.0.5.zip
-          // https://github.com/electron/electron/archive/v16.0.5.tar.gz
-          if (item.tarball_url) {
-            items.push({
-              name: `${item.tag_name}.tar.gz`,
-              isDir: false,
-              url: `https://github.com/${this.binaryConfig.repo}/archive/${item.tag_name}.tar.gz`,
-              size: '-',
-              date: item.published_at,
-            });
-          }
-          if (item.zipball_url) {
-            items.push({
-              name: `${item.tag_name}.zip`,
-              isDir: false,
-              url: `https://github.com/${this.binaryConfig.repo}/archive/${item.tag_name}.zip`,
-              size: '-',
-              date: item.published_at,
-            });
-          }
+          items = this.formatItems(item);
           break;
         }
       }
