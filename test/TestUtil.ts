3231a27e59f21d82a8859244298bb8348e04018f
TZ | 天猪 <atian25@qq.com>
2022-11-03 15:05:17 +0000

    feat: devcontainer with codespaces

--- test/TestUtil.ts
+++ test/TestUtil.ts
@@ -49,7 +49,7 @@ export class TestUtil {
       host: process.env.MYSQL_HOST || '127.0.0.1',
       port: process.env.MYSQL_PORT || 3306,
       user: process.env.MYSQL_USER || 'root',
-      password: process.env.MYSQL_PASSWORD,
+      password: process.env.MYSQL_PASSWORD || '',
       multipleStatements: true,
     };
   }
