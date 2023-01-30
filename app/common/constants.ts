c7048fbd8b27fedbe5ec6cbab09f8e455c81c395
hezhengxu2018 <35598090+hezhengxu2018@users.noreply.github.com>
2023-01-30 13:24:30 +0800

    feat: support proxy mode

--- app/common/constants.ts
+++ app/common/constants.ts
@@ -1,3 +1,4 @@
 export const BUG_VERSIONS = 'bug-versions';
 export const LATEST_TAG = 'latest';
 export const GLOBAL_WORKER = 'GLOBAL_WORKER';
+export const PROXY_MODE_CACHED_PACKAGE_DIR_NAME = 'proxy-mode-cached-packages';
