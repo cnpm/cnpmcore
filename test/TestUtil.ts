f7344eb90fbf432f2fa8439e24aecbb7717bc840
fengmk2 <fengmk2@gmail.com>
2023-02-13 09:54:09 +0800

    test: use 127.0.0.1 instead of localhost to connect local db (#404)

--- test/TestUtil.ts
+++ test/TestUtil.ts
@@ -42,7 +42,7 @@ export class TestUtil {
 
   static getMySqlConfig() {
     return {
-      host: process.env.MYSQL_HOST || 'localhost',
+      host: process.env.MYSQL_HOST || '127.0.0.1',
       port: process.env.MYSQL_PORT || 3306,
       user: process.env.MYSQL_USER || 'root',
       password: process.env.MYSQL_PASSWORD,
