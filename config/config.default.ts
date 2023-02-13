f9398b01234353409b5dc5229ff8c252de35771d
fengmk2 <fengmk2@gmail.com>
2023-02-13 09:33:16 +0800

    test: use 127.0.0.1 instead of localhost to connect local db

--- config/config.default.ts
+++ config/config.default.ts
@@ -91,7 +91,7 @@ export default (appInfo: EggAppConfig) => {
   config.orm = {
     client: 'mysql',
     database: process.env.MYSQL_DATABASE || 'cnpmcore',
-    host: process.env.MYSQL_HOST || 'localhost',
+    host: process.env.MYSQL_HOST || '127.0.0.1',
     port: process.env.MYSQL_PORT || 3306,
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD,
