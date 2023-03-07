77fdf6133698d54562e0250e4dee800782e065b6
lanxiu.lwl <lanxiu.lwl@alipay.com>
2023-03-07 15:16:25 +0800

    use crypto instead node-rsa

--- app/common/CryptoUtil.ts
+++ app/common/CryptoUtil.ts
@@ -1,24 +1,33 @@
-import NodeRSA from 'node-rsa';
+import { generateKeyPairSync, publicEncrypt, privateDecrypt, constants } from 'crypto';
 
 // generate rsa key pair
 export function genRSAKeys(): { publicKey: string, privateKey: string } {
-  const key = new NodeRSA({ b: 512 });
-  key.setOptions({ encryptionScheme: 'pkcs1' });
-  const publicKey = key.exportKey('pkcs8-public');
-  const privateKey = key.exportKey('pkcs8-private');
+  const key = generateKeyPairSync('rsa', {
+    modulusLength: 512,
+  });
+  const publicKey = key.publicKey.export({
+    type: 'pkcs1',
+    format: 'pem',
+  }).toString('base64');
+  const privateKey = key.privateKey.export({
+    type: 'pkcs1',
+    format: 'pem',
+  }).toString('base64');
   return { publicKey, privateKey };
 }
 
 // encrypt rsa private key
 export function encryptRSA(publicKey: string, data: string): string {
-  const pubKey = new NodeRSA(publicKey, 'pkcs8-public');
-  pubKey.setOptions({ encryptionScheme: 'pkcs1' });
-  return pubKey.encrypt(data, 'base64');
+  return publicEncrypt({
+    key: publicKey,
+    padding: constants.RSA_PKCS1_PADDING,
+  }, Buffer.from(data, 'utf8')).toString('base64');
 }
 
 // decrypt rsa private key
-export function decryptRSA(privateKey: string, data: string): string {
-  const priKey = new NodeRSA(privateKey, 'pkcs8-private');
-  priKey.setOptions({ encryptionScheme: 'pkcs1' });
-  return priKey.decrypt(Buffer.from(data, 'base64'), 'utf8');
+export function decryptRSA(privateKey: string, data: string) {
+  return privateDecrypt({
+    key: privateKey,
+    padding: constants.RSA_PKCS1_PADDING,
+  }, Buffer.from(data, 'base64')).toString('utf8');
 }
