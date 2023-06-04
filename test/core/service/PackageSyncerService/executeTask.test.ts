bead9d1a79ff78e7ef2850eb282068b1bf4f0278
Tony <hzx.tony@hotmail.com>
2023-06-04 20:23:38 +0800

    test: fix test.

--- test/core/service/PackageSyncerService/executeTask.test.ts
+++ test/core/service/PackageSyncerService/executeTask.test.ts
@@ -1218,7 +1218,7 @@ describe('test/core/service/PackageSyncerService/executeTask.test.ts', () => {
         persist: false,
       });
       const name = '@cnpmcore/test-sync-package-has-two-versions';
-      await packageSyncerService.createTask(name, { specificVersions: [ '1.0.0' ] });
+      await packageSyncerService.createTask(name, { specificVersions: [ '1.0.0' ], forbiddenAutoSyncLatestVersion: true });
       const task = await packageSyncerService.findExecuteTask();
       assert(task);
       assert.equal(task.targetName, name);
@@ -1230,7 +1230,7 @@ describe('test/core/service/PackageSyncerService/executeTask.test.ts', () => {
       assert(app.mockAgent().pendingInterceptors().length === 1);
     });
 
-    it('should sync all specific versions.', async () => {
+    it('should sync specific versions and latest version.', async () => {
       app.mockHttpclient('https://registry.npmjs.org/%40cnpmcore%2Ftest-sync-package-has-two-versions', 'GET', {
         data: '{"_id":"@cnpmcore/test-sync-package-has-two-versions","_rev":"4-541287ae0a14039fea89ac08fa5ec53d","name":"@cnpmcore/test-sync-package-has-two-versions","dist-tags":{"latest":"2.0.0","next":"2.0.0"},"versions":{"1.0.0":{"name":"@cnpmcore/test-sync-package-has-two-versions","version":"1.0.0","description":"cnpmcore local test package","main":"index.js","scripts":{"test":"echo \\"hello\\""},"author":"","license":"MIT","gitHead":"60cfb1cf401f87a60a1b0dfd7ee739f98ffd7847","_id":"@cnpmcore/test-sync-package-has-two-versions@1.0.0","_nodeVersion":"16.13.1","_npmVersion":"8.1.2","dist":{"integrity":"sha512-WR0T96H8t7ss1FK8GWPPblx+usbjU4bNGRjMHS9t/oVA5DgJDxitydPSFPeIUtXciyekI7R47do9Lc3GgC4P5A==","shasum":"2ddc6ee93b92be6d64139fb1a631d2610f43e946","tarball":"https://registry.npmjs.org/@cnpmcore/test-sync-package-has-two-versions/-/test-sync-package-has-two-versions-1.0.0.tgz","fileCount":2,"unpackedSize":238,"signatures":[{"keyid":"SHA256:jl3bwswu80PjjokCgh0o2w5c2U4LhQAE57gj9cz1kzA","sig":"MEYCIQDj5Ui2GU8nVmHFk0hCt/i3gPW9eQdOCZgKzpAlkvERwQIhAPZ0NCefLoEfOpnbdKAUr7Ng9Sy6FMnTsDxDaM2dQHNw"}]},"_npmUser":{"name":"fengmk2","email":"fengmk2@gmail.com"},"directories":{},"maintainers":[{"name":"fengmk2","email":"fengmk2@gmail.com"}],"_npmOperationalInternal":{"host":"s3://npm-registry-packages","tmp":"tmp/test-sync-package-has-two-versions_1.0.0_1639442699824_0.6948988437963031"},"_hasShrinkwrap":false},"2.0.0":{"name":"@cnpmcore/test-sync-package-has-two-versions","version":"2.0.0","description":"cnpmcore local test package","main":"index.js","scripts":{"test":"echo \\"hello\\""},"author":"","license":"MIT","gitHead":"60cfb1cf401f87a60a1b0dfd7ee739f98ffd7847","_id":"@cnpmcore/test-sync-package-has-two-versions@2.0.0","_nodeVersion":"16.13.1","_npmVersion":"8.1.2","dist":{"integrity":"sha512-qgHLQzXq+VN7q0JWibeBYrqb3Iajl4lpVuxlQstclRz4ejujfDFswBGSXmCv9FyIIdmSAe5bZo0oHQLsod3pAA==","shasum":"891eb8e08ceadbd86e75b6d66f31f7e5a28a8d68","tarball":"https://registry.npmjs.org/@cnpmcore/test-sync-package-has-two-versions/-/test-sync-package-has-two-versions-2.0.0.tgz","fileCount":2,"unpackedSize":238,"signatures":[{"keyid":"SHA256:jl3bwswu80PjjokCgh0o2w5c2U4LhQAE57gj9cz1kzA","sig":"MEQCIAWVz7mIHF23Gq4a+Swsj2ZSdn87991HcE1+fQm8shNCAiByOIuhaZAbo9hct24qYf7FWqx6Lyluo+Rpnrn91//Ibg=="}]},"_npmUser":{"name":"fengmk2","email":"fengmk2@gmail.com"},"directories":{},"maintainers":[{"name":"fengmk2","email":"fengmk2@gmail.com"}],"_npmOperationalInternal":{"host":"s3://npm-registry-packages","tmp":"tmp/test-sync-package-has-two-versions_2.0.0_1639442732240_0.33204392278137207"},"_hasShrinkwrap":false}},"time":{"created":"2021-12-14T00:44:59.775Z","1.0.0":"2021-12-14T00:44:59.940Z","modified":"2022-05-23T02:33:52.613Z","2.0.0":"2021-12-14T00:45:32.457Z"},"maintainers":[{"email":"killa07071201@gmail.com","name":"killagu"},{"email":"fengmk2@gmail.com","name":"fengmk2"}],"description":"cnpmcore local test package","license":"MIT","readme":"ERROR: No README data found!","readmeFilename":""}',
         persist: false,
@@ -1244,7 +1244,7 @@ describe('test/core/service/PackageSyncerService/executeTask.test.ts', () => {
         persist: false,
       });
       const name = '@cnpmcore/test-sync-package-has-two-versions';
-      await packageSyncerService.createTask(name, { specificVersions: [ '1.0.0', '2.0.0' ] });
+      await packageSyncerService.createTask(name, { specificVersions: [ '1.0.0' ] });
       const task = await packageSyncerService.findExecuteTask();
       assert(task);
       assert.equal(task.targetName, name);
@@ -1258,8 +1258,8 @@ describe('test/core/service/PackageSyncerService/executeTask.test.ts', () => {
 
     it('should append specific version to waiting task.', async () => {
       const name = '@cnpmcore/test-sync-package-has-two-versions';
-      await packageSyncerService.createTask(name, { specificVersions: [ '1.0.0' ] });
-      await packageSyncerService.createTask(name, { specificVersions: [ '2.0.0' ] });
+      await packageSyncerService.createTask(name, { specificVersions: [ '1.0.0' ], forbiddenAutoSyncLatestVersion: true });
+      await packageSyncerService.createTask(name, { specificVersions: [ '2.0.0' ], forbiddenAutoSyncLatestVersion: true });
       const task = await packageSyncerService.findExecuteTask();
       assert(task);
       assert.equal(task.targetName, name);
