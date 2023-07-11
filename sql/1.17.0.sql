69926408cf45f1435cbefe0dcdadc825fa657b09
hezhengxu <hzx.tony@hotmail.com>
2023-07-11 17:16:09 +0800

    feat: init proxy file repo.

--- sql/1.17.0.sql
+++ sql/1.17.0.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS `proxy_mode_cached_files` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `gmt_create` datetime(3) NOT NULL COMMENT 'create time',
   `gmt_modified` datetime(3) NOT NULL COMMENT 'modify time',
-  `target_name` varchar(214) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT '' COMMENT '@scope/package name',
-  `file_type` varchar(20) NOT NULL DEFAULT '' COMMENT 'file type',
-  `file_path` varchar(512) NOT NULL DEFAULT '' COMMENT 'nfs file path',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `target_name` varchar(214) NOT NULL COMMENT '@scope/package or @scope/package/version',
+  `file_type` varchar(20) NOT NULL COMMENT 'file type',
+  `file_path` varchar(512) NOT NULL COMMENT 'nfs file path',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_proxy_mode_file_path` (`file_path`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci COMMENT='proxy mode cached files';
