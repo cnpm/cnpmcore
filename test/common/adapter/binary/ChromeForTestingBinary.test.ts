289c7a0b4ddd8db85578a7528c51fdfaba659ace
fengmk2 <fengmk2@gmail.com>
2023-09-19 23:07:47 +0800

    feat: sync all crhome for test binaries
    closes https://github.com/cnpm/cnpmcore/issues/591

--- test/common/adapter/binary/ChromeForTestingBinary.test.ts
+++ test/common/adapter/binary/ChromeForTestingBinary.test.ts
@@ -1,6 +1,7 @@
 import assert from 'assert';
 import { app } from 'egg-mock/bootstrap';
 import { ChromeForTestingBinary } from '../../../../app/common/adapter/binary/ChromeForTestingBinary';
+import { TestUtil } from '../../../../test/TestUtil';
 
 describe('test/common/adapter/binary/ChromeForTestingBinary.test.ts', () => {
   let binary: ChromeForTestingBinary;
@@ -9,21 +10,43 @@ describe('test/common/adapter/binary/ChromeForTestingBinary.test.ts', () => {
   });
   describe('fetch()', () => {
     it('should work for chrome binary', async () => {
+      assert.equal(ChromeForTestingBinary.lastTimestamp, '');
+      app.mockHttpclient('https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json', 'GET', {
+        data: await TestUtil.readFixturesFile('chrome-for-testing/known-good-versions-with-downloads.json'),
+        persist: false,
+      });
       const result = await binary.fetch('/');
-      const latestVersion = result?.items?.[0].name;
+      const latestVersion = result!.items![result!.items.length - 1].name;
       assert(latestVersion);
+      assert.equal(latestVersion, '119.0.6008.0/');
 
       const platformRes = await binary.fetch(`/${latestVersion}`);
       const platforms = platformRes?.items.map(item => item.name);
       assert(platforms);
+      assert.deepEqual(platforms, [ 'linux64/', 'mac-arm64/', 'mac-x64/', 'win32/', 'win64/' ]);
 
       for (const platform of platforms) {
         const versionRes = await binary.fetch(`/${latestVersion}${platform}`);
         const versions = versionRes?.items.map(item => item.name);
-        assert.equal(versions?.length, 1);
+        assert.equal(versions?.length, 3);
         assert(versionRes?.items[0].name);
         assert.equal(versionRes?.items[0].isDir, false);
+        assert.match(versionRes?.items[0].name, /^chrome\-/);
+        assert.match(versionRes?.items[1].name, /^chromedriver\-/);
+        assert.match(versionRes?.items[2].name, /^chrome\-headless\-shell\-/);
       }
+      assert(ChromeForTestingBinary.lastTimestamp);
+    });
+
+    it('should return empty when timestamp is not changed', async () => {
+      assert(ChromeForTestingBinary.lastTimestamp);
+      await binary.initFetch();
+      app.mockHttpclient('https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json', 'GET', {
+        data: await TestUtil.readFixturesFile('chrome-for-testing/known-good-versions-with-downloads.json'),
+        persist: false,
+      });
+      const result = await binary.fetch('/');
+      assert.equal(result?.items.length, 0);
     });
   });
 });
