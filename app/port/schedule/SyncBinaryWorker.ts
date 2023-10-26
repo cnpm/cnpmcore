22d401ee1f103a9702448d4749f0028a676eddc0
fengmk2 <fengmk2@gmail.com>
2023-10-26 15:38:10 +0800

    feat: ignore network error to error log (#600)

--- app/port/schedule/SyncBinaryWorker.ts
+++ app/port/schedule/SyncBinaryWorker.ts
@@ -29,7 +29,20 @@ export class SyncBinaryWorker {
     this.logger.info('[SyncBinaryWorker:executeTask:start] taskId: %s, targetName: %s, attempts: %s, params: %j, updatedAt: %s, delay %sms',
       task.taskId, task.targetName, task.attempts, task.data, task.updatedAt,
       startTime - task.updatedAt.getTime());
-    await this.binarySyncerService.executeTask(task);
+    try {
+      await this.binarySyncerService.executeTask(task);
+    } catch (err) {
+      const use = Date.now() - startTime;
+      this.logger.warn('[SyncBinaryWorker:executeTask:error] taskId: %s, targetName: %s, use %sms, error: %s',
+        task.taskId, task.targetName, use, err.message);
+      if (err.name === 'ConnectTimeoutError'
+        || err.name === 'HttpClientRequestTimeoutError') {
+        this.logger.warn(err);
+      } else {
+        this.logger.error(err);
+      }
+      return;
+    }
     const use = Date.now() - startTime;
     this.logger.info('[SyncBinaryWorker:executeTask:success] taskId: %s, targetName: %s, use %sms',
       task.taskId, task.targetName, use);
