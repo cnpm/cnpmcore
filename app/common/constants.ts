f2f85fe959e4ed688b836b29ada8248b9b0fe62c
Tony <hzx.tony@hotmail.com>
2024-03-30 20:43:01 +0800

    fix: rename constant

--- app/common/constants.ts
+++ app/common/constants.ts
@@ -1,8 +1,9 @@
 export const BUG_VERSIONS = 'bug-versions';
 export const LATEST_TAG = 'latest';
 export const GLOBAL_WORKER = 'GLOBAL_WORKER';
+export const PROXY_CACHE_DIR_NAME = 'proxy-cache-packages';
 export const NOT_IMPLEMENTED_PATH = [ '/-/npm/v1/security/audits/quick', '/-/npm/v1/security/advisories/bulk' ];
-export const PROXY_MODE_CACHED_PACKAGE_DIR_NAME = 'proxy-mode-cached-packages';
+
 export enum SyncMode {
   none = 'none',
   admin = 'admin',
