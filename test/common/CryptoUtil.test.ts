442dc401b0c982bbd716bd2eef60563f7006dc73
fengmk2 <suqian.yf@antgroup.com>
2024-05-16 13:38:11 +0800

    fix: remove CVE-2023-46809 revert config
    use node-rsa instead of RSA_PKCS1_PADDING
    
    revert https://github.com/cnpm/cnpmcore/pull/650
    
    run test on Node.js 22

---
+++ test/common/CryptoUtil.test.ts
@@ -0,0 +1,24 @@
+import { strict as assert } from 'node:assert';
+import { genRSAKeys, encryptRSA, decryptRSA } from '../../app/common/CryptoUtil';
+
+describe('test/common/CryptoUtil.test.ts', () => {
+  describe('genRSAKeys()', () => {
+    it('should work', () => {
+      const keys = genRSAKeys();
+      assert(keys.publicKey);
+      assert(keys.privateKey);
+      // console.log(keys);
+    });
+  });
+
+  describe('encryptRSA(), decryptRSA()', () => {
+    it('should work', () => {
+      const keys = genRSAKeys();
+      // const plainText = 'hello world 中文😄';
+      const plainText = 'hello world 中文';
+      const encryptText = encryptRSA(keys.publicKey, plainText);
+      // console.log(encryptText);
+      assert.equal(decryptRSA(keys.privateKey, encryptText), plainText);
+    });
+  });
+});
