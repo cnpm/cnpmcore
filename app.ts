dab340cde7e8d91e6380eaea0d7aad2a5f7fe4b2
fengmk2 <suqian.yf@antgroup.com>
2024-11-30 14:39:13 +0800

    f

--- app.ts
+++ app.ts
@@ -1,81 +1,11 @@
 import path from 'path';
 import { readFile } from 'fs/promises';
-import { Application, Context } from 'egg';
-import {
-  HttpClient as RawHttpClient,
-  RequestURL as HttpClientRequestURL,
-  RequestOptions,
-} from 'urllib';
-import ms from 'ms';
+import { Application } from 'egg';
 import { ChangesStreamService } from './app/core/service/ChangesStreamService';
 
 declare module 'egg' {
   interface Application {
     binaryHTML: string;
-  }
-}
-
-interface HttpClientRequestOptions extends RequestOptions {
-  ctx?: Context;
-  tracer?: unknown;
-}
-
-const SSRF_HTTPCLIENT = Symbol('SSRF_HTTPCLIENT');
-
-class HttpClient extends RawHttpClient {
-  readonly #app: Application & { tracer?: unknown };
-
-  constructor(app: Application, options?: any) {
-    normalizeConfig(app);
-    options = {
-      ...app.config.httpclient,
-      ...options,
-    };
-    super({
-      app,
-      defaultArgs: options.request,
-      allowH2: options.allowH2,
-      // use on egg-security ssrf
-      // https://github.com/eggjs/egg-security/blob/master/lib/extend/safe_curl.js#L11
-      checkAddress: options.checkAddress,
-    } as any);
-    this.#app = app;
-  }
-
-  async request<T = any>(url: HttpClientRequestURL, options?: HttpClientRequestOptions) {
-    options = options ?? {};
-    if (options.ctx?.tracer) {
-      options.tracer = options.ctx.tracer;
-    } else {
-      options.tracer = options.tracer ?? this.#app.tracer;
-    }
-    return await super.request<T>(url, options);
-  }
-
-  async curl<T = any>(url: HttpClientRequestURL, options?: HttpClientRequestOptions) {
-    return await this.request<T>(url, options);
-  }
-
-  async safeCurl<T = any>(url: HttpClientRequestURL, options: any = {}) {
-    if (!this[SSRF_HTTPCLIENT]) {
-      const ssrfConfig = this.#app.config.security.ssrf;
-      if (ssrfConfig?.checkAddress) {
-        options.checkAddress = ssrfConfig.checkAddress;
-      } else {
-        this.#app.logger.warn('[egg-security] please configure `config.security.ssrf` first');
-      }
-      this[SSRF_HTTPCLIENT] = new HttpClient(this.#app, {
-        checkAddress: ssrfConfig.checkAddress,
-      });
-    }
-    return await this[SSRF_HTTPCLIENT].request<T>(url, options);
-  }
-}
-
-function normalizeConfig(app: Application) {
-  const config = app.config.httpclient;
-  if (typeof config.request?.timeout === 'string') {
-    config.request.timeout = ms(config.request.timeout as string);
   }
 }
 
@@ -85,8 +15,6 @@ export default class CnpmcoreAppHook {
   constructor(app: Application) {
     this.app = app;
     this.app.binaryHTML = '';
-    Reflect.set(app, 'HttpClient', HttpClient);
-    Reflect.set(app, 'HttpClientNext', HttpClient);
   }
 
   async configWillLoad() {
