433e6921ffaa389e91c1986dbe1a26bf821b2c08
fengmk2 <suqian.yf@antgroup.com>
2024-11-30 15:03:42 +0800

    handle timeout error in one place

---
+++ app/common/ErrorUtil.ts
@@ -0,0 +1,26 @@
+const TimeoutErrorNames = [
+  'HttpClientRequestTimeoutError',
+  'HttpClientConnectTimeoutError',
+  'ConnectionError',
+  'ConnectTimeoutError',
+  'BodyTimeoutError',
+];
+
+export function isTimeoutError(err: Error) {
+  if (TimeoutErrorNames.includes(err.name)) {
+    return true;
+  }
+  if (err instanceof AggregateError && err.errors) {
+    for (const subError of err.errors) {
+      if (TimeoutErrorNames.includes(subError.name)) {
+        return true;
+      }
+    }
+  }
+  if (err.cause instanceof Error) {
+    if (TimeoutErrorNames.includes(err.cause.name)) {
+      return true;
+    }
+  }
+  return false;
+}
