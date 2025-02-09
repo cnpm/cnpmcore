c10ec85871ef025d0b954dce1c99c56535d55eb6
fengmk2 <fengmk2@gmail.com>
2025-02-09 14:35:36 +0800

    f

--- test/port/controller/package/DownloadPackageVersionTarController.test.ts
+++ test/port/controller/package/DownloadPackageVersionTarController.test.ts
@@ -329,12 +329,12 @@ describe('test/port/controller/package/DownloadPackageVersionTarController.test.
       });
       let res = await app.httpRequest()
         .get(`/${name}/download/${name}-1.0.0.tgz`);
-      assert(res.status === 302);
-      assert(res.headers.location === `https://cdn.mock.com/packages/${name}/1.0.0/${name}-1.0.0.tgz`);
+      assert.equal(res.status, 302);
+      assert.equal(res.headers.location, `https://cdn.mock.com/packages/${name}/1.0.0/${name}-1.0.0.tgz`);
       res = await app.httpRequest()
         .get(`/${scopedName}/download/${scopedName}-1.0.0.tgz`);
-      assert(res.status === 302);
-      assert(res.headers.location === `https://cdn.mock.com/packages/${scopedName}/1.0.0/${name}-1.0.0.tgz`);
+      assert.equal(res.status, 302);
+      assert.equal(res.headers.location, `https://cdn.mock.com/packages/${scopedName}/1.0.0/${name}-1.0.0.tgz`);
     });
 
     it('should download a version tar with streaming success', async () => {
