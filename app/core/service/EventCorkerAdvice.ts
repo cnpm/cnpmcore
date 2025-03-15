f47fc60c00fd5e8c9c777eccaa6d3ff8cc9de490
killagu <zhubin.gzb@antgroup.com>
2025-03-16 01:16:38 +0800

    perf: refactor AsyncTimer advice to Singleton

--- app/core/service/EventCorkerAdvice.ts
+++ app/core/service/EventCorkerAdvice.ts
@@ -1,9 +1,11 @@
 import type { ContextEventBus } from '@eggjs/tegg';
-import { Inject } from '@eggjs/tegg';
+import { Inject, ObjectInitType } from '@eggjs/tegg';
 import type { IAdvice } from '@eggjs/tegg/aop';
 import { Advice } from '@eggjs/tegg/aop';
 
-@Advice()
+@Advice({
+  initType: ObjectInitType.CONTEXT,
+})
 export class EventCorkAdvice implements IAdvice {
   @Inject()
   private eventBus: ContextEventBus;
