f19652343eceab85d8761d8f849ce09beb736b17
MK <fengmk2@gmail.com>
2025-10-07 22:01:43 +0800

    FIXUP

--- .docker/build.sh
+++ .docker/build.sh
@@ -2,6 +2,6 @@
 
 node -v && npm -v \
   && npm install -g npminstall --registry=https://registry.npmmirror.com \
-  && NODE_DEBUG=egg-bin* npminstall -c \
+  && NODE_DEBUG=egg-bin*,egg/bin* npminstall -c --foreground-scripts \
   && npm run tsc \
   && npmupdate -c --production
