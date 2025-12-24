f27060cff54c918e0f7543eb8a209ca8f73d2d3f
killagu <zhubin.gzb@antgroup.com>
2025-12-25 00:48:23 +0800

    call gc
    node --expose-gc benchmark.js
    
    use kill -s USR2

--- benchmark/stream_download/benchmark.js
+++ benchmark/stream_download/benchmark.js
@@ -2,6 +2,7 @@ const urllib = require('urllib');
 const fs = require('fs');
 const crypto = require('crypto');
 const path = require('path');
+const { setTimeout } = require('timers/promises');
 
 async function downloadAndUpload() {
   const tmpFilePath = path.join(__dirname, `${crypto.randomUUID()}.txt`);
@@ -15,8 +16,20 @@ async function downloadAndUpload() {
   await fs.promises.rm(tmpFilePath);
 }
 
+let downloading = true;
 (async () => {
   while (true) {
-    await downloadAndUpload();
+    if (downloading) {
+      await downloadAndUpload();
+    } else {
+      await setTimeout(100);
+      if (globalThis.gc) {
+        globalThis.gc();
+      }
+    }
   }
 })();
+
+process.on('SIGUSR2', () => {
+  downloading = !downloading;
+});
